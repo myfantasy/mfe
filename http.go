bd6ff46008a7090808075957587de6ee53134ed0
Ilya Scherbina <sch@myfantasy,ru>
2018-05-30 18:38:12 +0300

    http un marshal json

--- http.go
+++ http.go
@@ -1,11 +1,23 @@
 package mfe
 
 import (
+	"encoding/json"
 	"fmt"
 	"sort"
 
 	fasthttp "github.com/valyala/fasthttp"
 )
+
+// InputData -- Входные параметры запроса
+type InputData struct {
+	path    string
+	method  string
+	get     map[string]string
+	post    map[string]string
+	json    Variant
+	cookie  map[string]string
+	headers map[string]string
+}
 
 var rDir map[string]func(ctx *fasthttp.RequestCtx)
 var rMap map[int]map[string]func(ctx *fasthttp.RequestCtx)
@@ -23,11 +35,21 @@ func ServerHeaderSet(server string) {
 
 // AddDirectRoute -- Добавить роут точное имя
 func AddDirectRoute(uri string, f func(ctx *fasthttp.RequestCtx)) {
+	uri = "/" + uri
+	if rDir == nil {
+		rDir = make(map[string]func(ctx *fasthttp.RequestCtx))
+	}
 	rDir[uri] = f
 }
 
 // AddRoute -- Добавить роут
 func AddRoute(uri string, f func(ctx *fasthttp.RequestCtx)) {
+	uri = "/" + uri
+	if rMap == nil {
+		rMap = make(map[int]map[string]func(ctx *fasthttp.RequestCtx))
+		lMap = make(map[int]struct{})
+	}
+
 	l := len(uri)
 	if _, ok := lMap[l]; !ok {
 		lMap[l] = struct{}{}
@@ -106,5 +128,85 @@ func DisplayInputHandler(ctx *fasthttp.RequestCtx) {
 		fmt.Fprintln(ctx, string(value))
 	})
 
+	fmt.Fprintln(ctx, "<br>cookie:")
+	ctx.Request.Header.VisitAllCookie(func(key, value []byte) {
+		fmt.Fprintln(ctx, "<br>")
+		fmt.Fprint(ctx, string(key))
+		fmt.Fprint(ctx, " : ")
+		fmt.Fprintln(ctx, string(value))
+	})
+
 	ctx.Response.Header.SetContentType("text/html;charset=utf-8")
+}
+
+// InputDataGet получить все значения из
+func InputDataGet(ctx *fasthttp.RequestCtx) (id InputData) {
+	id = InputData{}
+	id.get = map[string]string{}
+	id.post = map[string]string{}
+	id.headers = map[string]string{}
+	id.cookie = map[string]string{}
+
+	id.path = string(ctx.Path())
+	id.method = string(ctx.Method())
+
+	ctx.QueryArgs().VisitAll(func(key, value []byte) {
+		id.get[string(key)] = string(value)
+	})
+
+	ctx.Request.Header.VisitAll(func(key, value []byte) {
+		id.headers[string(key)] = string(value)
+	})
+
+	ctx.PostArgs().VisitAll(func(key, value []byte) {
+		id.post[string(key)] = string(value)
+	})
+
+	ctx.Request.Header.VisitAllCookie(func(key, value []byte) {
+		id.cookie[string(key)] = string(value)
+	})
+
+	var vv Variant
+	if err := json.Unmarshal(ctx.PostBody(), &vv); err == nil {
+		id.json = vv
+	} else {
+		id.json = VariantNewNull()
+	}
+
+	return id
+}
+
+// Path Get
+func (id InputData) Path() string {
+	return id.path
+}
+
+// Method Get
+func (id InputData) Method() string {
+	return id.method
+}
+
+// Get Get
+func (id InputData) Get() map[string]string {
+	return id.get
+}
+
+// Post Get
+func (id InputData) Post() map[string]string {
+	return id.post
+}
+
+// Cookie Get
+func (id InputData) Cookie() map[string]string {
+	return id.cookie
+}
+
+// Headers Get
+func (id InputData) Headers() map[string]string {
+	return id.headers
+}
+
+// JSONData Get
+func (id InputData) JSONData() Variant {
+	return id.json
 }
